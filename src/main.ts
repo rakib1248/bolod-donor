88eed30e339d3bd83bdf9aab4836ef8278fec650
rakib1248 <rakib25059159@gmail.com>
2025-10-16 21:29:40 +0300

    update All  file

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // ✅ CORS enable
+  app.enableCors({
+    origin: '*', // সব origin allow করবে
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
